4bfb675fbc34f65c98c7c0417b8b7bddeaebdd2d
marumaru1019 <ryu.ryusei401@gmail.com>
2021-05-30 18:40:08 +0900

    delete settings

--- kintone_program/kintone_01.py
+++ kintone_program/kintone_01.py
@@ -1,13 +1,6 @@
 import requests
 import time
 import logging
-
-URL="https://61r93edyrsd5.cybozu.com/k/v1/"
-APPID=1
-APPID2=7
-API_TOKEN="uxuJRsQ7jkunko02d9nVQnDSyIJtDmEe2xSGXFEp,pDfygdA7GUMQ7iTLEkljfaww5EDiKkjOOFcor4bM"
-API_TOKEN2="pDfygdA7GUMQ7iTLEkljfaww5EDiKkjOOFcor4bM"
-
 # import data into kinto db
 def q_params(line_name,line_id,contents,category="non",sub1="",sub2="",name="",ID=""):
     url_flag=0
@@ -66,7 +59,7 @@ def ret_API_data(api_flag=0):
         return URL,APPID2,API_TOKEN2
     else:
         return URL,APPID,API_TOKEN
-        
+
 def q_post(url, api_token, params):
     headers = {"X-Cybozu-API-Token": api_token,
                "Content-Type": "application/json"}

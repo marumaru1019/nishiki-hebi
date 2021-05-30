8e8664764b815972ee6144fbf9820a2a4678653d
marumaru1019 <ryu.ryusei401@gmail.com>
2021-05-30 12:03:56 +0900

    detect word

--- Nisiki/decide_category.py
+++ Nisiki/decide_category.py
@@ -14,7 +14,7 @@ def decide_category(text):
     for token in doc:
         # token.lemma_は，分かち書きされた単語（の原型）
         # カテゴリと単語は自由に設定して下さい
-    
+
         if token.lemma_ in {'人事'}:
             category = '人事宛'
         elif token.lemma_ in {'同期'}:
@@ -30,18 +30,18 @@ def decide_category(text):
 
 def text_analyze(text):
     import collections
-    
+
     c = collections.Counter(text)
     print(c.most_common(20))
-    
+
 def emotion_analyzer(text):
     #ポジネガ分析をする関数(MeCabが入るなら使える)
-    from mlask import MLAsk    
+    from mlask import MLAsk
 
     # mecab -Dで辞書のパスを確認し，引数に渡す(末尾のsys.dicは削除)
     emotion_analyzer = MLAsk('-d /usr/local/lib/mecab/dic/ipadic/')
     analyze = emotion_analyzer.analyze(text)
-    
+
     return analyze
 
 if __name__ == '__main__':
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     #category = analyze('同期は何人くらい居ますか？')
     #category = analyze('研修についてお聞きしたいです．')
 
-    #text_analyze('人事さんに質問です．会社の雰囲気を改めて教えて下さい．')   
+    #text_analyze('人事さんに質問です．会社の雰囲気を改めて教えて下さい．')
 
     analyze = emotion_analyzer('ゆにしすちゃん嫌い！')
     print(analyze)
